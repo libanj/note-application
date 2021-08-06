752f8c1772f3a275a614371c9c75a86a7abb6181
Liban <liban1808@gmail.com>
2021-08-06 21:56:39 +0100

    add new note

--- src/Controller/NotesController.php
+++ src/Controller/NotesController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Notes;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 
 class NotesController extends AbstractController
 {
@@ -26,4 +30,19 @@ class NotesController extends AbstractController
 		return $this->render('create.html.twig');
 	}
 
+	public function saveNote(Request $req): Response
+	{
+		$title = $req->request->get('title');
+		$body = $req->request->get('body');
+
+		$note = new Notes();
+		$note->setTitle($title);
+		$note->setBody($body);
+		
+		$entityManager = $this->getDoctrine()->getManager();
+		$entityManager->persist($note);
+		$entityManager->flush();
+		
+		return $this->render('create.html.twig');
+	}
 }
